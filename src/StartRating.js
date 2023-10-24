d7d0b53e600678184b7a538bb6b265655f135117
AkhiSharmin <akhisharmin100@gmail.com>
2023-10-25 00:34:23 +0600

    added start rating

---
+++ src/StartRating.js
@@ -0,0 +1,15 @@
+export default function StarRating() {
+
+    const containerStyle = {
+        display: 'flex',
+        alignItems: 'center',
+        gap: '16px'
+    }
+
+    return <div style={containerStyle}>
+        <div>
+            {Array.from({ length: 5 }, (_, i) => <span>S{i + 1}</span>)}
+        </div>
+        <p>10</p>
+    </div>
+}
