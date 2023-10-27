ab67ff04cc5f030f9b65fe50cc7c415434f4c154
AkhiSharmin <akhisharmin100@gmail.com>
2023-10-27 17:38:46 +0600

    added anather day

--- src/index.js
+++ src/index.js
@@ -6,10 +6,20 @@ import ReactDOM from "react-dom/client";
 
 import StarRating from "./StartRating";
 
+function Test() {
+  return <div>
+    <StarRating color="blue" maxRating={10} />
+    <p>This movies was rated X start</p>
+  </div>
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating maxRating={5} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']} />
+    <StarRating size={24} color="red" className='test' defaultRating={2} />
+
+    <Test />
   </React.StrictMode>
 );
